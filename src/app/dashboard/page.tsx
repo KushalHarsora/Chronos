45154b7668412d7e6bf4f824506c52e37b87b8d7
Kushal Harsora <kushalharsora13@gmail.com>
2024-09-09 13:53:31 +0530

    some changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-import { GearIcon, HamburgerMenuIcon, MagnifyingGlassIcon, HomeIcon, TokensIcon } from "@radix-ui/react-icons"
+import { FramerLogoIcon, GearIcon } from "@radix-ui/react-icons"
 import axios from "axios"
 import { useRouter } from "next/navigation"
 import React from "react"
@@ -38,115 +38,37 @@ const Dashboard = () => {
     return (
         <React.Fragment>
             <main className=" h-screen w-screen flex justify-center items-center">
-                {/* Navbar */}
-                <section className=" fixed z-0 h-[10vh] w-screen top-0 left-0 flex flex-row justify-between px-[10%] items-center border-b-[1.5px] bg-white/75 backdrop-blur-lg">
-                    <div>
-                        <Button variant={'ghost'} onClick={() => { router.push('/') }} className=" font-mono font-semibold text-xl hover:bg-transparent">
+                {/* Left Section */}
+                <section className=" h-screen w-1/4 absolute left-0 top-0 bg-slate-50 flex flex-col justify-center items-center">
+                    <div className=" h-[10vh] w-full absolute top-0 left-0 flex justify-center items-center">
+                        <Button variant={'ghost'} onClick={() => { router.push('/') }} className=" font-mono font-semibold text-2xl hover:bg-transparent">
                             Chronos.
                         </Button>
                     </div>
-                    <div className=" flex flex-row max-lg:hidden">
-                        <Button
-                            variant={'link'}
-                            className=" w-full gap-2 underline"
-                            onClick={() => { router.push('/dashboard') }}
-                        >
-                            <HomeIcon />
-                            Dashboard
-                        </Button>
-                        <Button
-                            variant={'destructive'}
-                            className=" bg-red-600 w-1/2 hover:bg-red-600"
-                            onClick={handlelogout}
-                        >
-                            Sign Out
-                        </Button>
-                    </div>
-                    <div className=" lg:hidden">
-                        <Sheet>
-                            <SheetTrigger asChild>
-                                <Button variant={'ghost'} className=" hover:bg-transparent">
-                                    <HamburgerMenuIcon />
-                                </Button>
-                            </SheetTrigger>
-                            <SheetContent>
-                                <div className=" w-full h-full flex flex-col justify-between items-center overflow-hidden">
-                                    <div className=" mt-[5vh] h-2/6 w-full flex flex-col justify-evenly items-center">
-                                        <Button
-                                            variant={'link'}
-                                            className=" w-full gap-2 underline"
-                                            onClick={() => { }}
-                                        >
-                                            <MagnifyingGlassIcon />
-                                            Search
-                                        </Button>
-                                        <Button
-                                            variant={'link'}
-                                            className=" w-full gap-2 underline"
-                                            onClick={() => { router.push('/dashboard') }}
-                                        >
-                                            <HomeIcon />
-                                            Dashboard
-                                        </Button>
-                                        <Button
-                                            variant={'link'}
-                                            className=" w-full gap-2 underline"
-                                            onClick={() => { router.push('/setting') }}
-                                        >
-                                            <GearIcon />
-                                            Settings
-                                        </Button>
-                                        <Button
-                                            variant={'link'}
-                                            className=" w-full gap-2 underline"
-                                            onClick={() => { router.push('/') }}
-                                        >
-                                            <TokensIcon />
-                                            New Page
-                                        </Button>
-                                    </div>
-                                    <Button
-                                        variant={'destructive'}
-                                        className=" bg-red-600 w-1/2 hover:bg-red-600"
-                                        onClick={handlelogout}
-                                    >
-                                        Sign Out
-                                    </Button>
-                                </div>
-                            </SheetContent>
-                        </Sheet>
-                    </div>
-                </section>
-                <section className=" absolute top-[10vh] left-0 h-[90vh] w-full flex justify-center items-center">
-                    <div className=" absolute left-0 top-0 w-1/5 h-[90vh] bg-gray-50 flex flex-col justify-start items-start">
-                        <Button
-                            variant={'link'}
-                            className=" w-fit gap-2 underline"
-                            onClick={() => { }}
-                        >
-                            <MagnifyingGlassIcon />
-                            Search
-                        </Button>
-                        <Button
-                            variant={'link'}
-                            className=" w-fit gap-2 underline"
-                            onClick={() => { router.push('/setting') }}
-                        >
-                            <GearIcon />
-                            Settings
-                        </Button>
-                        <Button
-                            variant={'link'}
-                            className=" w-fit gap-2 underline"
-                            onClick={() => { router.push('/') }}
-                        >
-                            <TokensIcon />
+                    <div className=" h-[90vh] w-full absolute top-[10vh] left-0 px-4 flex flex-col justify-between items-center">
+                        <div className=" h-fit w-full flex flex-col justify-start items-start gap-2 px-4 mt-4">
+                        <Button variant={'link'} onClick={() => {}} className=" font-mono font-medium text-base hover:bg-transparent underline gap-2">
+                            <FramerLogoIcon />
                             New Page
                         </Button>
+                        <Button variant={'link'} onClick={() => {}} className=" font-mono font-medium text-base hover:bg-transparent underline gap-2">
+                            <GearIcon />
+                            Setting
+                        </Button>
+                        </div>
+                        <div className=" h-fit w-full flex justify-center items-center mb-10">
+                            <Button
+                                variant={'destructive'}
+                                className=" text-base w-2/5"
+                                onClick={handlelogout}
+                            >
+                                Sign out
+                            </Button>
+                        </div>
                     </div>
-                    <div>
+                </section>
+                <section className=" h-screen w-3/4 absolute left-1/4 top-0 bg-white">
 
-                    </div>
                 </section>
             </main>
         </React.Fragment>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,10 +67,10 @@ export default function Home() {
       {/* Main */}
       <section className=" absolute top-[10vh] left-0 z-0 h-[90vh] w-screen flex flex-row justify-center items-center gap-1 max-lg:flex-col">
         <div className=" w-1/2 h-[90vh] flex flex-col justify-center items-center gap-3 max-lg:w-screen max-lg:mx-2 max-lg:h-[30vh] max-lg:absolute max-lg:top-[10vh] max-lg:left-0">
-          <h1 className=" text-4xl font-bold max-lg:text-3xl max-lg:hidden">
+          <h1 className=" text-4xl font-bold max-lg:text-3xl">
             Welcome to <span className=" font-extrabold underline">Chronos</span>.
           </h1>
-          <TextGenerateEffect className=" flex justify-center items-center px-4" words={words} />
+          <TextGenerateEffect className=" flex justify-center items-center px-4 max-lg:text-sm" words={words} />
           <Button
             className=" gap-2"
             onClick={() => { router.push('/sign-up') }}
@@ -80,7 +80,7 @@ export default function Home() {
           </Button>
         </div>
         <div className=" w-1/2 h-[90vh] flex justify-center items-center max-lg:w-screen max-lg:mx-2 max-lg:h-[50vh] max-lg:absolute max-lg:top-[40vh] max-lg:left-0 max-lg:overflow-hidden">
-          <div className=" w-fit h-fit shadow-lg shadow-gray-300 overflow-hidden px-6 rounded-2xl max-lg:overflow-hidden">
+          <div className=" w-fit h-fit overflow-hidden px-6 rounded-2xl max-lg:overflow-hidden">
             <Image src={'logo.svg'} height={400} width={500} priority alt={"logo"} />
           </div>
         </div>
